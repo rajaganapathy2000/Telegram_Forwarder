8842ac4a04273b1a1e455bc3b78a1d949617f271
Raja Ganapathy <79167443+rajaganapathy2000@users.noreply.github.com>
2021-04-02 15:24:13 +0530

    Update sample_config.py

--- forwarder/sample_config.py
+++ forwarder/sample_config.py
@@ -10,16 +10,16 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
-    OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
+    API_KEY = "1708696362:AAHCFv6RJUXpfsEHkewwkwTHH1abvLlw5mw"  # API key obtained from BotFather
+    OWNER_ID = "1340559309"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
-    FROM_CHATS = [-1001234704297 ]  # List of chat id's to forward messages from
-    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to
+    FROM_CHATS = [-1001185679066]  # List of chat id's to forward messages from
+    TO_CHATS = [-1001390605266]  # List of chat id's to forward messages to
 
     # FOR WEBHOOKS
     WEBHOOK = False
-    IP_ADDRESS = "127.0.0.1"  # Use "0.0.0.0" if using Heroku
+    IP_ADDRESS = "0.0.0.0"  # Use "0.0.0.0" if using Heroku
     URL = None  # The URL that the bot should listen to for updates
     PORT = 5000  # Port to listen on for webhooks
     CERT_PATH = None  # Path to certificate file
